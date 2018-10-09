7e6b39178171b6e6881c6b1ec5c2a6044e78d710
echchaari <echchaari.brahim@gmail.com>
2018-10-09 13:36:24 -0500

    print in console

--- Main.js
+++ Main.js
@@ -87,7 +87,7 @@ function SimpleEvenAdding(n){
     return splitStr.join(' '); 
  }
  
- document.write(letterCapitalize("hi! my name is brahim"));
+ console.log(letterCapitalize("hi! my name is Brahim"));
 
 
   //---------------Question 6-------------//
@@ -96,7 +96,7 @@ function SimpleEvenAdding(n){
     return s.split('').reverse().join('');
   }
 
-  document.write(simpleReverse('Brahim Likes JavaScript '));
+  console.log(simpleReverse('Brahim Likes JavaScript'));
 
  //---------------Question 7-------------//
 
